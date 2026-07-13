7c6f9763577f01dc6bbd95a381e6d3b27159714d
chlrhhan <chlrhhan@outlook.com>
2026-07-13 15:48:17 +0900

    fix: merge module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { UserModule } from './user/user.module';
 import { PayModule } from './pay/pay.module';
-
-@Module({
-  imports: [UserModule, PayModule],
-=======
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [PrismaModule, ProductModule, CartModule, OrderModule],
->>>>>>> 1f4385e3e4b5c2904161fe1a9140a70c9ade2930
+  imports: [
+    PrismaModule,
+    ProductModule,
+    CartModule,
+    OrderModule,
+    UserModule,
+    PayModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
