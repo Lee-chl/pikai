4311f837e74da60e0cb37a3e3b49861a2edba667
mynsun <alstjs3022@naver.com>
2026-07-13 15:41:15 +0900

    feat: module user, pay add

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PayModule } from './pay/pay.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PayModule],
   controllers: [AppController],
   providers: [AppService],
 })
