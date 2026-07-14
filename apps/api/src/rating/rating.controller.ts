c5d87f35a56f6f791e3d0a0a2c7f036d121a5821
iyeji <chlrhhan@gmail.com>
2026-07-14 18:09:41 +0900

    feat: rating create api

--- apps/api/src/rating/rating.controller.ts
+++ apps/api/src/rating/rating.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RatingService } from './rating.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { RatingEntity } from './entities/rating.entity';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Rating } from '@prisma/client';
 
 @Controller('rating')
+@ApiTags('Rating')
 export class RatingController {
   constructor(private readonly ratingService: RatingService) {}
 
   @Post()
-  create(@Body() createRatingDto: CreateRatingDto) {
-    return this.ratingService.create(createRatingDto);
+  @ApiOperation({ summary: '제품에 대한 별점 추가' })
+  @ApiResponse({ type: RatingEntity })
+  async create(@Body() createRatingDto: CreateRatingDto) {
+    const create_rating: Rating =
+      await this.ratingService.create(createRatingDto);
+    return new RatingEntity(create_rating);
   }
 
   @Get()
--- apps/api/src/rating/rating.service.ts
+++ apps/api/src/rating/rating.service.ts
@@ -1,11 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class RatingService {
-  create(createRatingDto: CreateRatingDto) {
-    return 'This action adds a new rating';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createRatingDto: CreateRatingDto) {
+    // 이미 등록된 별점인지 와 상품과 유저가 있는 지 확인
+    const [existRating, detail_color, user] = await Promise.all([
+      this.prisma.rating.findUnique({
+        where: {
+          detail_color_id_user_id: {
+            detail_color_id: createRatingDto.detail_color_id,
+            user_id: createRatingDto.user_id,
+          },
+        },
+      }),
+      this.prisma.detailColor.findUnique({
+        where: { id: createRatingDto.detail_color_id },
+      }),
+      this.prisma.user.findUnique({ where: { id: createRatingDto.user_id } }),
+    ]);
+
+    if (!existRating) {
+      throw new ConflictException(`해당 제품의 별점이 이미 존재합니다.}`);
+    }
+
+    if (!detail_color) {
+      throw new NotFoundException(
+        `${createRatingDto.detail_color_id}은 존재 하지 않은 제품 컬러입니다.`,
+      );
+    }
+    if (!user) {
+      throw new NotFoundException(
+        `${createRatingDto.user_id}는 존재하지 않는 유저입니다.`,
+      );
+    }
+
+    return this.prisma.rating.create({ data: createRatingDto });
   }
 
   findAll() {
