28d8efd27b95b2f339c21ab7fe9988c4de9ea613
lindapark777 <lindapark311@outlook.kr>
2026-07-15 15:41:58 +0900

    feat: products findAll,findOne add

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
@@ -18,8 +27,8 @@ export class ProductController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.findOne(id);
   }
 
   @Patch(':id')
--- apps/api/src/product/product.service.ts
+++ apps/api/src/product/product.service.ts
@@ -1,19 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    return await this.prisma.product.create({
+      data: {
+        color_main_image: createProductDto.color_main_image,
+        color_detail_image: createProductDto.color_detail_image,
+        name: createProductDto.name,
+        hash_tag: createProductDto.hash_tag,
+        price: createProductDto.price,
+        is_sale: createProductDto.is_sale ?? false,
+        category_id: createProductDto.category_id,
+        brand_id: createProductDto.brand_id,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all product`;
+  //상품 전체 조회
+  async findAll() {
+    return this.prisma.product.findMany({
+      orderBy: {
+        id: 'desc',
+      },
+      include: {
+        category: true,
+        brand: true,
+        detail_color: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  //상품 단일 조회
+  async findOne(id: number) {
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      include: {
+        category: true,
+        brand: true,
+        detail_color: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
