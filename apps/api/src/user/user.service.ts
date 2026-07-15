ff5596b11400550432644b3c3d6aa340dfe19fa4
YJ <chlrhhan@gmail.com>
2026-07-15 22:12:20 +0900

    feat: user getOne and update 부분 추가

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { checkPermissionId } from '../common/check-permission';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+  async findOne(id: number, currentUserId: number) {
+    // 본인 id 만 조회 가능
+    checkPermissionId(id, currentUserId);
+    const existUser = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!existUser) {
+      throw new NotFoundException(`[${id}] 유저가 없어요`);
+    }
+
+    const { password, ...result } = existUser;
+
+    return result;
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+    currentUserId: number,
+  ) {
+    // 유저 존재 확인
+    await this.findOne(id, currentUserId);
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+    const { password, ...result } = user;
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return result;
   }
 }
