3885926fad4ce8478b4ecdebf3d638b3fa437e07
lindapark777 <lindapark311@outlook.kr>
2026-07-15 11:06:22 +0900

    feat: create product dto add

--- apps/api/src/product/dto/create-product.dto.ts
+++ apps/api/src/product/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsInt, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({ example: '상품 메인 사진 파일명' })
+  @IsString()
+  color_main_image: string;
+
+  @ApiProperty({ example: '상품 상세 사진 파일명' })
+  @IsString()
+  color_detail_image: string;
+
+  @ApiProperty({ example: '상품 이름' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: '해시태그' })
+  @IsArray()
+  @IsString({ each: true })
+  hash_tag: string[];
+
+  @ApiProperty({ example: '정가: 10000', description: '원단위' })
+  @IsInt()
+  @Min(0)
+  price: number;
+
+  @ApiProperty({ example: '판매 여부' })
+  @IsBoolean()
+  is_sale: boolean;
+}
