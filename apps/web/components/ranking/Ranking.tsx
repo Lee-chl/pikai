13602f10c445589d471f804a9e93707362c51a47
mynsun <alstjs3022@naver.com>
2026-07-17 18:34:06 +0900

    feat: main page 사용자 별 랭킹 출력 및 배너 부분 기능 추가

---
+++ apps/web/components/ranking/Ranking.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import RankingItem from "./Ranking-item";
+import styles from "./Ranking.module.css";
+
+interface Tone {
+  detail_color_id: number;
+  sale_count: number;
+  detailColor: {
+    id: number;
+    color_name: string;
+    color_image: string;
+    products: {
+      id: number;
+      name: string;
+      price: number;
+      is_sale: boolean;
+    };
+  };
+}
+
+export default async function Ranking() {
+  const res = await fetch(`${process.env.URL}/tone`);
+  const tones: Tone[] = await res.json();
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>베스트 상품</h2>
+      <div className={styles.list}>
+        {tones.map((tone, index) => (
+          <Link
+            key={tone.detail_color_id}
+            href={`/product/${tone.detailColor.products.id}`}
+          >
+            <RankingItem
+              key={tone.detail_color_id}
+              tone={tone}
+              rank={index + 1}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
