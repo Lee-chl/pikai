864a1a20602cbff137c7b4856c9b27315755e9f0
lindapark777 <lindapark311@outlook.kr>
2026-07-18 12:31:37 +0900

    feat: web product Pagination코드 추가

---
+++ apps/web/app/(service)/product/components/Pagination.tsx
@@ -0,0 +1,12 @@
+export default function Pagination() {
+  return (
+    <div>
+      <div>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">4</button>
+      </div>
+    </div>
+  );
+}
