6f6cc1d8b323185a931a643cc25b2868d625dd04
YJ <chlrhhan@gmail.com>
2026-07-19 04:09:59 +0900

    feat: 별점 페이지 카드 css 적용 및 호버 시 h s l 보이게 설정 + 상세로 이동 삭제 기능 구현

--- apps/web/components/rating/Rating-item.tsx
+++ apps/web/components/rating/Rating-item.tsx
@@ -3,6 +3,9 @@
 import { RatingItemType } from "../../types/ratingType";
 import { Constants } from "../../common/constants";
 import Image from "next/image";
+import { Star } from "lucide-react";
+import styles from "./Rating-item.module.css";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface RatingItemProps {
   ratingItem: RatingItemType;
@@ -11,30 +14,66 @@ interface RatingItemProps {
   setComDel?: (id: number) => void;
 }
 
+type CssVariables = React.CSSProperties & {
+  "--color": string;
+};
 export default function RatingItem({
   ratingItem,
   isSelected,
   onSelect,
   setComDel,
 }: RatingItemProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const color_main_image = ratingItem.detail_color.products.color_main_image;
-  const color_name = ratingItem.detail_color.color_name;
   const imageURL = `${Constants.image_url}/${color_main_image}`;
-  const name = ratingItem.detail_color.products.name;
+  const { color_name, h, s, l } = ratingItem.detail_color;
+  const { name } = ratingItem.detail_color.products;
   const { id, star_rating } = ratingItem;
 
+  const handleOneClick = () => {
+    if (onSelect) {
+      onSelect(id, !isSelected);
+    } else if (setComDel) {
+      setComDel(id);
+    }
+  };
+  const handleTwoClick = () => {
+    if (onSelect) {
+      const userId = searchParams.get("userId");
+      router.push(`/rating/${id.toString()}?userId=${userId}`);
+    }
+  };
+
   return (
-    <div>
-      <div>
+    <article
+      className={`${styles.card} ${isSelected ? styles.selected : ""}`}
+      /*  변수 주입해서 css에서 사용*/
+      style={
+        {
+          "--color": `${h} ${s}% ${l}%`,
+        } as CssVariables
+      }
+      onClick={handleOneClick}
+      onDoubleClick={handleTwoClick}
+    >
+      <div className={styles.imageContainer}>
         <Image
           src={imageURL}
-          width={200}
-          height={200}
+          fill
+          sizes={"(max-width: 768px) 50vw, 20vw"}
+          priority={false}
           alt={`${name}-${color_name} 의 사진입니다.`}
         />
       </div>
-      <div>{name}</div>
-      <div>{color_name}</div>
-    </div>
+      <div className={styles.infoWrapper}>
+        {name}
+        <div className={styles.itemName}>{color_name}</div>
+      </div>
+      <div className={styles.rating}>
+        <Star size={16} fill="#ffa39a" color="#ffa39a" />
+        <span>{star_rating}</span>
+      </div>
+    </article>
   );
 }
