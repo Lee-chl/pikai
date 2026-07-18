beceb5c9b9a4a5d96e81cf20627a6799ac143c4e
lindapark777 <lindapark311@outlook.kr>
2026-07-18 12:32:26 +0900

    feat: web product SortMenu 코드 추가

---
+++ apps/web/app/(service)/product/components/SortMenu.tsx
@@ -0,0 +1,12 @@
+export default function SortMenu() {
+  return (
+    <div>
+      <div>
+        <a href="#">판매순</a>
+        <a href="#">최신등록순</a>
+        <a href="#">낮은 가격순</a>
+        <a href="#">높은 가격순</a>
+      </div>
+    </div>
+  );
+}
