02b97fb72dc8b9e14500979f1de1eb1c96fcb044
lindapark777 <lindapark311@outlook.kr>
2026-07-19 12:35:06 +0900

    feat:ProductList 생성 이미지,상품명, 가격 코드 추가

--- apps/web/app/(service)/product/components/ProductList.tsx
+++ apps/web/app/(service)/product/components/ProductList.tsx
@@ -1,4 +1,3 @@
-import ProductItem from "@/components/product-item";
 import { ProductItemType } from "@/types/productItemType";
 
 interface ProductListProps {
@@ -6,14 +5,34 @@ interface ProductListProps {
 }
 
 export default function ProductList({ products }: ProductListProps) {
-  if (!products || products.length === 0) {
-    return <p>상품이 없습니다.</p>;
+  if (products.length === 0) {
+    return <p>등록된 상품이 없습니다.</p>;
   }
 
   return (
-    <div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(5, 1fr)",
+        gap: "20px",
+      }}
+    >
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <div key={product.id}>
+          <img
+            src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${product.color_main_image}`}
+            alt={product.name}
+            style={{
+              width: "100%",
+              height: "200px",
+              objectFit: "cover",
+            }}
+          />
+
+          <h4>{product.name}</h4>
+
+          <p>{product.price.toLocaleString()}원</p>
+        </div>
       ))}
     </div>
   );
