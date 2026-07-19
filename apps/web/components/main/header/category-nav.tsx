881f588ad842106124bcaf34a8d7063421835ba3
mynsun <alstjs3022@naver.com>
2026-07-19 14:40:49 +0900

    fix: category href 경로 수정

--- apps/web/components/main/header/category-nav.tsx
+++ apps/web/components/main/header/category-nav.tsx
@@ -19,7 +19,7 @@ export default async function CategoryNav() {
         {categories.map((category) => (
           <Link
             key={category.id}
-            href={`/product/${category.id}`}
+            href={`/product/category/${category.id}`}
             className={styles.link}
           >
             {category.name}
