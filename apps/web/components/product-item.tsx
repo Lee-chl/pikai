798be55551f1ea57fe63bea3c725f7e16209d1b2
lindapark777 <lindapark311@outlook.kr>
2026-07-19 14:07:10 +0900

    test: product item 테스트 추가 삭제

--- apps/web/components/product-item.tsx
+++ apps/web/components/product-item.tsx
@@ -13,6 +13,7 @@ export default function ProductItem({ product }: ProductItemProps) {
 
       <div>
         <div>{product.name}</div>
+
         <div>{product.price.toLocaleString()}원</div>
       </div>
     </div>
