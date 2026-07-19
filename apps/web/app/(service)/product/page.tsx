4b97839bce9a395572d51dfc83c4fc7b4ebafba3
lindapark777 <lindapark311@outlook.kr>
2026-07-19 12:44:24 +0900

    feat: product page 코드 추가

---
+++ apps/web/app/(service)/product/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { ProductItemType } from "@/types/productItemType";
+import Pagination from "./components/Pagination";
+import ProductList from "./components/ProductList";
+import SortMenu from "./components/SortMenu";
+import { useEffect, useState } from "react";
+import { ProductSortType } from "@/types/productSortTyps";
+
+interface ProductResponseType {
+  items: ProductItemType[];
+  total: number;
+  page: number;
+  limit: number;
+}
+
+export default function Page() {
+  const [products, setProducts] = useState<ProductItemType[]>([]);
+
+  const [selectedSort, setSelectedSort] = useState<ProductSortType>("latest");
+
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState("");
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleSortChange = (sort: ProductSortType) => {
+    setSelectedSort(sort);
+    setCurrentPage(1);
+  };
+
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+      setError("");
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACK_URL}/products?page=${currentPage}&limit=10&sort=${selectedSort}`,
+      );
+
+      if (!response.ok) {
+        throw new Error("상품 목록을 가져오지 못했습니다.");
+      }
+
+      const data: ProductResponseType = await response.json();
+
+      setProducts(data.items);
+    } catch (error) {
+      console.error(error);
+
+      setError("상품 목록을 불러오는 중 오류가 발생했습니다.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_BACK_URL);
+    fetchProducts();
+  }, [selectedSort, currentPage]);
+
+  return (
+    <div>
+      <h3>전체상품 </h3>
+      <SortMenu selectedSort={selectedSort} onSortChange={handleSortChange} />
+
+      <ProductList products={products} />
+
+      <Pagination currentPage={currentPage} onPageChange={setCurrentPage} />
+    </div>
+  );
+}
