4d699591baea9cb15e75d29b9d4d92c5b2fedeb8
lindapark777 <lindapark311@outlook.kr>
2026-07-21 10:21:30 +0900

    feat: create cart dto add

--- apps/api/src/cart/dto/create-cart.dto.ts
+++ apps/api/src/cart/dto/create-cart.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCartDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt } from 'class-validator';
+
+export class CreateCartDto {
+  @ApiProperty({ example: 1, description: '회원 id' })
+  @IsInt()
+  userId: number;
+}
