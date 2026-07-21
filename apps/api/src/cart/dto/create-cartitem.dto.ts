436d77f2009ab598d57ec0ba2a7d55f74281b2bf
lindapark777 <lindapark311@outlook.kr>
2026-07-21 10:19:39 +0900

    feat: create cartitem dto add

---
+++ apps/api/src/cart/dto/create-cartitem.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Min } from 'class-validator';
+
+export class CreateCartitemDto {
+  @ApiProperty({
+    example: 1,
+    description: '장바구니 ID',
+  })
+  @IsInt()
+  cart_id: number;
+
+  @ApiProperty({
+    example: 3,
+    description: '상품 색상(DetailProduct) ID',
+  })
+  @IsInt()
+  detail_color_id: number;
+
+  @ApiProperty({
+    example: 2,
+    description: '수량',
+    default: 1,
+  })
+  @IsInt()
+  @Min(1)
+  quantity: number;
+
+  @ApiProperty({
+    example: 15000,
+    description: '장바구니 담을 당시 상품 가격',
+  })
+  @IsInt()
+  @Min(0)
+  price: number;
+}
