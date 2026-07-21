028a4caa87ad8d944b7c7f6226b8d13e15a08b28
lindapark777 <lindapark311@outlook.kr>
2026-07-21 10:22:36 +0900

    feat: cart controller ts add

--- apps/api/src/cart/cart.controller.ts
+++ apps/api/src/cart/cart.controller.ts
@@ -1,34 +1,108 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateCartitemDto } from './dto/create-cartitem.dto';
+import { UpdateCartitemDto } from './dto/update-cartitem.dto';
+import { UpdateCartitemSelectDto } from './dto/update-cartitemselect.dto';
 
+@ApiTags('장바구니')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  // Cart
+
   @Post()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartService.create(createCartDto);
+  @ApiOperation({
+    summary: '장바구니 생성',
+    description: '회원 id를 받아 장바구니를 생성합니다.',
+  })
+  createCart(@Body() createCartDto: CreateCartDto) {
+    return this.cartService.createCart(createCartDto);
   }
 
+  @ApiOperation({
+    summary: '모든 회원의 장바구니 조회 (관리자)',
+    description: '모든 장바구니를 조회합니다.',
+  })
   @Get()
   findAll() {
     return this.cartService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cartService.findOne(+id);
+  @Get('user/:userId')
+  @ApiOperation({
+    summary: '회원 장바구니 조회',
+    description: '회원 ID를 이용해 장바구니와 장바구니 상품을 조회합니다.',
+  })
+  findCartByUserId(@Param('userId', ParseIntPipe) userId: number) {
+    return this.cartService.findCartByUserId(userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
-    return this.cartService.update(+id, updateCartDto);
+  // CartItem
+
+  @Post('items')
+  @ApiOperation({
+    summary: '장바구니 상품 추가',
+    description:
+      '상품을 장바구니에 추가합니다. 이미 있는 상품이면 수량을 증가시킵니다.',
+  })
+  createCartitem(@Body() createCartitemDto: CreateCartitemDto) {
+    return this.cartService.createCartitem(createCartitemDto);
+  }
+  @Patch('items/:cartItemId')
+  @ApiOperation({
+    summary: '장바구니 상품 수량 수정',
+    description: '장바구니 상품의 수량을 수정합니다.',
+  })
+  updateCartitem(
+    @Param('cartItemId', ParseIntPipe) cartItemId: number,
+    @Body() updateCartitemDto: UpdateCartitemDto,
+  ) {
+    return this.cartService.updateCartitem(cartItemId, updateCartitemDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cartService.remove(+id);
+  @Patch('items/:cartItemId/select')
+  @ApiOperation({
+    summary: '장바구니 상품 선택 상태 수정',
+    description: '상품의 선택 또는 선택 해제 상태를 수정합니다.',
+  })
+  updateCartitemSelect(
+    @Param('cartItemId', ParseIntPipe) cartItemId: number,
+    @Body() updateCartitemSelectDto: UpdateCartitemSelectDto,
+  ) {
+    return this.cartService.updateCartitemSelect(
+      cartItemId,
+      updateCartitemSelectDto,
+    );
+  }
+
+  @Delete('items/:cartItemId')
+  @ApiOperation({
+    summary: '장바구니 상품 삭제',
+    description: '장바구니에서 상품 하나를 삭제합니다.',
+  })
+  deleteCartitem(@Param('cartItemId', ParseIntPipe) cartItemId: number) {
+    return this.cartService.deleteCartitem(cartItemId);
+  }
+
+  @Delete(':cartId/items')
+  @ApiOperation({
+    summary: '장바구니 비우기',
+    description: '특정 장바구니에 담긴 모든 상품을 삭제합니다.',
+  })
+  clearCart(@Param('cartId', ParseIntPipe) cartId: number) {
+    return this.cartService.clearCart(cartId);
   }
 }
