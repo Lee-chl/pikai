3deb4ea1808b8079324fc03a150825fc694aa671
mynsun <alstjs3022@naver.com>
2026-07-21 11:52:46 +0900

    fix: 주문 내역 link href 수정

--- apps/web/components/main/header/UserMenu.tsx
+++ apps/web/components/main/header/UserMenu.tsx
@@ -42,7 +42,7 @@ export default function UserMenu() {
   return (
     <div className={styles.container}>
       <Link href={`/cart?userId=${user.id}`}>장바구니</Link>
-      <Link href={`/order?userId=${user.id}`}>주문 내역</Link>
+      <Link href={`/order`}>주문 내역</Link>
       <Link href={`/user/mypage/${user.id}`}>마이페이지</Link>
       <Link href={`/rating?userId=${user.id}`}>나만의 화장대</Link>
       <button type="button">로그아웃</button>
