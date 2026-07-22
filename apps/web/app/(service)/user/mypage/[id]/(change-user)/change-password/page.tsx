4786c53f64ea98039e53acaab40f3eed32c1507c
mynsun <alstjs3022@naver.com>
2026-07-22 14:51:55 +0900

    feat: 비밀번호 변경 기능 구현

--- apps/web/app/(service)/user/mypage/[id]/(change-user)/change-password/page.tsx
+++ apps/web/app/(service)/user/mypage/[id]/(change-user)/change-password/page.tsx
@@ -1,11 +1,23 @@
-export default function ChangePassword({
+import { Constants } from "@/common/constants";
+import PasswordForm from "./passwordForm";
+import styles from "./page.module.css";
+
+export default async function ChangePassword({
   params,
 }: {
   params: Promise<{ id: number }>;
 }) {
+  const { id } = await params;
+
   return (
-    <div>
-      <p></p>
+    <div className={styles.container}>
+      <h2 className={styles.title}>비밀번호 변경</h2>
+
+      <p className={styles.description}>
+        현재 비밀번호를 입력한 후 새 비밀번호로 변경해주세요.
+      </p>
+
+      <PasswordForm id={id} />
     </div>
   );
 }
