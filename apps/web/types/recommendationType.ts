ccd0b06a52de7ad8c42e9ed8be01fc4aad61db0e
lindapark777 <lindapark311@outlook.kr>
2026-07-22 10:01:06 +0900

    feat: recommendationType add

---
+++ apps/web/types/recommendationType.ts
@@ -0,0 +1,9 @@
+// AI 추천 API에서 전달받는 응답 데이터 타입
+export interface RecommendationResponseType {
+  score: number;
+  messageType: string;
+  title: string;
+  message: string;
+  recommend: boolean;
+  isMock: boolean;
+}
