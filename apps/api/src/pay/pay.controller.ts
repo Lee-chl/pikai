69f9ea42a596455950b63e7b97ca185276fbc771
mynsun <alstjs3022@naver.com>
2026-07-27 12:17:34 +0900

    feat: 결제 create 기능 add

--- apps/api/src/pay/pay.controller.ts
+++ apps/api/src/pay/pay.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PayService } from './pay.service';
 import { CreatePayDto } from './dto/create-pay.dto';
 import { UpdatePayDto } from './dto/update-pay.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('pay')
 export class PayController {
   constructor(private readonly payService: PayService) {}
 
   @Post()
+  @ApiOperation({ summary: '결제' })
   create(@Body() createPayDto: CreatePayDto) {
-    return this.payService.create(createPayDto);
+    return this.payService.create(1, createPayDto);
   }
 
   @Get()
--- apps/api/src/pay/pay.service.ts
+++ apps/api/src/pay/pay.service.ts
@@ -1,11 +1,146 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePayDto } from './dto/create-pay.dto';
 import { UpdatePayDto } from './dto/update-pay.dto';
+import { NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PayService {
-  create(createPayDto: CreatePayDto) {
-    return 'This action adds a new pay';
+  constructor(private readonly prisma: PrismaService) {}
+
+  private async generateOrderId(): Promise<string> {
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const date = `${year}${month}${day}`;
+
+    //주문번호 생성
+    const lastOrder = await this.prisma.order.findFirst({
+      where: {
+        id: {
+          startsWith: date,
+        },
+      },
+      orderBy: {
+        id: 'desc',
+      },
+    });
+    let sequence = 1;
+
+    if (lastOrder) {
+      sequence = Number(lastOrder.id.slice(-4)) + 1;
+    }
+
+    return `${date}${String(sequence).padStart(4, '0')}`;
+  }
+
+  async create(userId: number, createPayDto: CreatePayDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('회원을 찾을 수 없습니다.');
+    }
+
+    const orderId = await this.generateOrderId();
+
+    return await this.prisma.$transaction(async (tx) => {
+      //상품 확인
+      for (const item of createPayDto.items) {
+        const product = await tx.detailProduct.findUnique({
+          where: {
+            id: item.detail_color_id,
+          },
+          include: {
+            products: true,
+          },
+        });
+        if (!product) {
+          throw new NotFoundException('상품을 찾을 수 없습니다.');
+        }
+
+        if (product.stock < item.quantity) {
+          throw new BadRequestException(
+            `${product.color_name}의 재고가 부족합니다.`,
+          );
+        }
+      }
+
+      const order = await tx.order.create({
+        data: {
+          id: orderId,
+          payment: createPayDto.payment,
+          delivery_info: createPayDto.delivery_info,
+          postal_code: createPayDto.postal_code,
+          delivery_inst: createPayDto.delivery_inst,
+          phone_number: createPayDto.phone_number,
+          recipient: createPayDto.recipient,
+          user_id: userId,
+        },
+      });
+
+      for (const item of createPayDto.items) {
+        const product = await tx.detailProduct.findUnique({
+          where: {
+            id: item.detail_color_id,
+          },
+          include: {
+            products: true,
+          },
+        });
+
+        await tx.orderItem.create({
+          data: {
+            order_id: order.id,
+            detail_color_id: item.detail_color_id,
+            quantity: item.quantity,
+            price: product!.products.price,
+          },
+        });
+
+        //재고 감소
+        await tx.detailProduct.update({
+          where: {
+            id: item.detail_color_id,
+          },
+          data: {
+            stock: {
+              decrement: item.quantity,
+            },
+          },
+        });
+      }
+
+      if (createPayDto.isCartOrder) {
+        const cart = await tx.cart.findUnique({
+          where: {
+            userId: userId,
+          },
+        });
+
+        if (!cart) {
+          throw new NotFoundException('장바구니를 찾을 수 없습니다.');
+        }
+
+        //장바구니에서 구매한 상품 삭제
+        for (const item of createPayDto.items) {
+          await tx.cartItem.deleteMany({
+            where: {
+              cart_id: cart.id,
+              detail_color_id: item.detail_color_id,
+            },
+          });
+        }
+      }
+
+      return order;
+    });
   }
 
   findAll() {
