8d8b936c58996dc92912aadbdceecd65f95e0b7c
YJ <chlrhhan@gmail.com>
2026-07-30 17:25:20 +0900

    feat: cart type

---
+++ apps/web/types/cartType.ts
@@ -0,0 +1,27 @@
+interface Product {
+  id: number;
+  color_main_image: string;
+  name: string;
+}
+
+interface DetailProduct {
+  id: number;
+  color_name: string;
+  stock: number;
+  product: Product;
+}
+
+interface CartItem {
+  id: number;
+  cart_id: number;
+  quantity: number;
+  is_selected: boolean;
+  is_now: boolean;
+  price: number;
+  detailProduct: DetailProduct[];
+}
+
+export interface Cart {
+  id: number;
+  cartItems: CartItem[];
+}
