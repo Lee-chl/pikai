31130198a669e22f89223904b45387a635881d8e
lindapark777 <lindapark311@outlook.kr>
2026-08-04 11:59:11 +0900

    fix: recommendation controller mock수정

--- apps/api/src/recommendations/recommendations.controller.ts
+++ apps/api/src/recommendations/recommendations.controller.ts
@@ -1,26 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RecommendationRequestDto } from './dto/recommendation-request.dto';
 import { RecommendationsService } from './recommendations.service';
-import { PersonalColor } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { CurrentUser } from 'src/common/current-user.decorator';
 
 @ApiTags('recommendations')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('recommendations')
 export class RecommendationsController {
   constructor(
     private readonly recommendationsService: RecommendationsService,
   ) {}
 
-  // 인증 기능 개발 전 임시 사용자
-  private mockUser = {
-    id: 1,
-    email: 'user@email.com',
-    isAdmin: false,
-    tone: PersonalColor.WARM,
-  };
-
   @Post('color')
-  recommendColor(@Body() dto: RecommendationRequestDto) {
-    return this.recommendationsService.recommendColor(dto, this.mockUser.id);
+  recommendColor(
+    @Body() dto: RecommendationRequestDto,
+    @CurrentUser('id') userId: number,
+  ) {
+    return this.recommendationsService.recommendColor(dto, userId);
   }
 }
