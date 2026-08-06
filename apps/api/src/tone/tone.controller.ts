38e79394b0faf0dca838ba0c02bf6e42136a48c8
YJ <chlrhhan@gmail.com>
2026-08-06 11:51:10 +0900

    fix: 톤 코드 수정

--- apps/api/src/tone/tone.controller.ts
+++ apps/api/src/tone/tone.controller.ts
@@ -3,7 +3,7 @@ import { ToneService } from './tone.service';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 import { CurrentUser } from 'src/common/current-user.decorator';
-import * as Common from '@repo/common';
+import { type jwtPayloadType } from '@repo/common';
 
 @Controller('tone')
 export class ToneController {
@@ -20,7 +20,7 @@ export class ToneController {
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @ApiOperation({ summary: '회원의 톤 별 베스트' })
-  async findTone(@CurrentUser() user: Common.jwtPayloadType) {
+  async findTone(@CurrentUser() user: jwtPayloadType) {
     if (!user) {
       throw new Error('유저 정보 없음');
     }
