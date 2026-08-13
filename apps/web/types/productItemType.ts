160c7a7a822e9f123bde8cbbfe1e2c9b947230cc
mynsun <alstjs3022@naver.com>
2026-08-13 15:20:17 +0900

    fix: 관리자페이지 상품 타입 추가

--- apps/web/types/productItemType.ts
+++ apps/web/types/productItemType.ts
@@ -4,3 +4,19 @@ export interface ProductItemType {
   name: string;
   price: number;
 }
+
+export interface ProductAdminType {
+  id: number;
+  color_main_image: string;
+  color_detail_image: string;
+  name: string;
+  price: number;
+  brand: {
+    id: number;
+    name: string;
+  };
+  category: {
+    id: number;
+    name: string;
+  };
+}
